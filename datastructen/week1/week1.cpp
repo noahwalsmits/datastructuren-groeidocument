89c259d01cdec8fce16300d241df735383bc2c09
noah <noahwalsmits@telfort.nl>
2023-04-24 16:04:00 +0200

    added 1-1

--- datastructen/week1/week1.cpp
+++ datastructen/week1/week1.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+bool search(std::string& s, char& key)
+{
+	if (s.find(key) == std::string::npos)
+	{
+		return false;
+	}
+	return true;
+}
+
+void exercise1_1()
+{
+	std::string enteredString = "hi";
+	char enteredKey = 'o';
+
+	std::cout << "enter a string to search through:" << std::endl;
+	std::cin >> enteredString;
+	std::cout << "enter a character to search for:" << std::endl;
+	std::cin >> enteredKey;
+
+	if (search(enteredString, enteredKey))
+	{
+		std::cout << enteredKey << " was found in " << enteredString << std::endl;
+	}
+	else
+	{
+		std::cout << enteredKey << " was not found in " << enteredString << std::endl;
+	}
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	exercise1_1();
 }
